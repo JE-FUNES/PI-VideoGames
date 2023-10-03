2919d7397f9aca2ec94b13e265379ab9341f7026
JE-FUNES <infoventacba@gmail.com>
2023-10-03 18:13:27 -0300

    css createGame

--- client/src/Views/Forms/CreateGame.jsx
+++ client/src/Views/Forms/CreateGame.jsx
@@ -95,16 +95,11 @@ const CreateGame = () => {
 
     return (
 
-        <div className={styles.container}>
+        <div className={styles.routeContainer}>
             
             <div className={styles.formContainer}>
                 <form onSubmit={handleSubmit}>
-            <div className={styles.title}>
-            <h1>Create your Game!</h1>
-            <br/>
-            <br/>
-            <br/>
-            </div>
+        
                     <div className={styles.formGroup}>
                         <label className={styles.label}>Name</label>
                         <input
