abd3baa81afc0fe2b2d5f67809549feee87d87c7
JE-FUNES <infoventacba@gmail.com>
2023-10-11 15:34:28 -0300

    validate 2 de 2 createGame

--- client/src/Views/Forms/constantesCreateGame.js
+++ client/src/Views/Forms/constantesCreateGame.js
@@ -34,13 +34,23 @@ export const validate = (input) => {
   let errors = {};
   if (!input.name) {
     errors.name = "Name is required";
-  }
-  if (!input.description) {
-    errors.description = "Description is required";
-  }
-  if (!input.image) {
-    errors.image = "Image is required";
-  }
+  } else if (input.name.length > 15) {
+      errors.name = "Name should be up to 15 characters long";
+    }
+    
+    if (!input.description) {
+      errors.description = "Description is required";
+    } else if (input.description.length > 1000) {
+      errors.description = "Description is too long (maximum 1000 characters).";
+    }
+    if (!input.image) {
+      errors.image = "Image is required";
+    } else {
+      const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+      if (!urlPattern.test(input.image)) {
+        errors.image = "Invalid URL format.";
+      }
+    }
   if (input.genres.length === 0) {
     errors.genres = "Select at least one";
   }
